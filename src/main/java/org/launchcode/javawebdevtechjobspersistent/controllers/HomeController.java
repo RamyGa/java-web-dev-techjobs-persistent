2e1b87c1c932440337d4f0eb0e2ce54bc2c7728d
Ramyga <ramygatie@gmail.com>
2021-03-25 20:53:24 -0500

    Added repository persistence to HomeController

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -1,6 +1,11 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.AccessType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -8,12 +13,21 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
  */
 @Controller
 public class HomeController {
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+
 
     @RequestMapping("")
     public String index(Model model) {
@@ -26,18 +40,35 @@ public class HomeController {
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
+        model.addAttribute("employers", employerRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-                                       Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
+                                       Errors errors, Model model, @RequestParam int employerId) {//, @RequestParam List<Integer> skills
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
             return "add";
         }
+
+        model.addAttribute("employer", employerRepository.findAll());
+
+
+            Employer employer = (Employer) employerRepository.findById(employerId).get();
+
+
+
+       newJob.setEmployer(employer);
+//       newJob.setSkills();
+        jobRepository.save(newJob);
+
+
+
+
+
 
         return "redirect:";
     }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,8 +1,14 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import org.hibernate.mapping.Join;
+
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -11,6 +17,10 @@ public class Employer extends AbstractEntity {
     @NotBlank(message = "location required")
     @Size(min = 4, max = 30, message = "location must be between 4 and 30 characters")
     private String location;
+
+    @OneToMany()
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer(@NotBlank(message = "location required") @Size(min = 4, max = 30, message = "location must be " +
             "between 4 and 30 characters") String location) {
